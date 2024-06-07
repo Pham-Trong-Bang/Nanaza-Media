703b414733148da5655a3124e64f6b0ca47c906a
Phạm Trọng Bằng <139265148+Pham-Trong-Bang@users.noreply.github.com>
2024-06-08 01:31:42 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function momenu() {
+    let menu = document.querySelector("#menu");
+    menu.style.display = 'block';
+}
+function tatmenu() {
+    let menu = document.querySelector("#menu");
+    menu.style.display = 'none';
+}
